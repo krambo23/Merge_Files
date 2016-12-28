a34a4f9ca8e9f77fefdc8173360658bef0a464d3
krambo23 <taj.keshav@gmail.com>
2016-12-29 00:02:48 +0400

    Added support for different types of encoding

--- merge_files.py
+++ merge_files.py
@@ -9,15 +9,20 @@ no_of_files = len(file_list)
 file_read = ""
 to_merge = ""
 
+# http://stackoverflow.com/a/12468274
+encoding_type = input("Input Encoding [Leave blank if UTF-8] : ")
+if encoding_type == "" :
+	encoding_type = "utf-8"
+
 # Reads each file and merges their content(s)
 for i in range(0, no_of_files) :
 	file_loc = folder_name + "/" + file_list[i]
-	file_read = open(file_loc, "r") # Opening "file_loc" in r mode
+	file_read = open(file_loc, "r", encoding = encoding_type, errors = "ignore") # Opening "file_loc" in r mode
 	to_merge += file_read.read() # Add content(s) of file into "to_merge"
 	file_read.close() # Closes "file_loc"
 
 # Writes to merged file
-file_write = open(merged_output, "w") # Opening merged_output in w mode
+file_write = open(merged_output, "w", encoding = encoding_type, errors = "ignore") # Opening merged_output in w mode
 file_write.write(to_merge) # Writes into merge_output the contents of "to_merge"
 file_write.close() # Closes to_merge
 
